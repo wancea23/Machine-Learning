427893b3d4d20d82ba8aae4b5d2442b42370cc98
wancea23 <imorosanu23@gmail.com>
2026-01-18 19:50:05 +0200

    Add Euclidean Distance function

--- K-Means Algorihtms/K-Means++.py
+++ K-Means Algorihtms/K-Means++.py
@@ -1,4 +1,8 @@
 import random
+import math
+
+def distance(p, c):
+    return math.sqrt((p[0] - c[0])**2 + (p[1] - c[1])**2)
 
 n_samples = int(input("Enter the number of samples: "))
 n_clusters = int(input("Enter the number of clusters: "))
@@ -11,7 +15,16 @@ for _ in range(n_samples):
     X.append((random.uniform(0, x_range), random.uniform(0, y_range)))
 
 centroids = []
-centroids.append(random.choice(X))
+centroids.append(random.choice(X))  #C1
 
-print("Initial Centroid:")
-print(centroids)
+distances = []
+for point in X:
+    min_dist = float('inf')
+    for c in centroids:
+        d = distance(point, c)
+        if d < min_dist:
+            min_dist = d
+    distances.append(min_dist)
+
+#print("Initial Centroid:")
+#print(centroids)
